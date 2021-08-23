848aefb4ece6c9a02417f1a848c6f7e0d5531b33
DisiKharismaP <disi.kharisma@gmail.com>
2021-08-23 14:08:23 +0700

    commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Carbon\Carbon;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
@@ -42,9 +43,6 @@ class UserController extends Controller
         $user->password = Hash::make($request->password);
         $user->save();
 
-        //generate token untuk user yang telah dibuat
-        $token = $user->createToken('api_token')->plainTextToken;
-
         //menampilkan response berisi user dan token
         //200 artinya sukses
         return response()->json(compact(['user', 'token']), 200);
@@ -53,38 +51,45 @@ class UserController extends Controller
     //Fungsi login user 
     public function loginUser(Request $request){
 
-        //ambil data email dan password saja
-        $data = $request->only(['email', 'password']);
+        //Mencari user dari inputan user menggunakan email
+        $user = User::where('email', $request['email'])->first();
 
-        //validasi data
-        $validator = Validator::make(
-            $data,
-            [
-                'email' => 'required|string|email',
-                'password' => 'required|string|min:8'
-            ]
-        );
-
-        //jika validator gagal, tampilkan error
-        if($validator->fails()){
-            $errors = $validator->errors();
-            return response()->json(compact('errors'), 400);
+        //Auth  attempt untuk mengecek apakah data(email dan password) sesuai
+        if($user&&Hash::check($request->password,$user->password)){
+            $token = str::random(60);
+            $user->remember_token = $token;
+            $user->save();
+            return response()->json([
+                "status"=>200,
+                "message"=>"success",
+                "token"=>$token,
+                "user"=>$user
+            ], 200);
         }
-
-        //Auth attempt untuk mengecek apakah data (email dan password) sesuai
-        if(Auth::attempt($data)){
-            $user = User::where('email', $data['email'])->first();
-            $token = $user->createToken('api_token')->plainTextToken;
-            return response()->json(compact(['user', 'token']), 200);
-        }
+        return response()->json([
+            "status"=>401,
+            "message"=>"failed",
+        ], 401);
     }
 
     //Fungsi logout, menghapus token dari database
     public function logoutUser(Request $request){
-        if($request->expectsJson()){
-            $isTokenDeleted = $request->User()->currentAccessToken()->delete();
-            return response()->json(compact('isTokenDeleted'), 200);
-        }
+       //mencari user menggunakan token
+       $user = User::where('remember_token', $request->bearerToken())->first();
+
+       //kalo user ada, jadiin token itu null
+       if($user){
+           $user->remember_token = null;
+           $user->save();
+           return response()->json([
+               "status"=>200,
+               "message"=>"success",
+           ], 200);
+       }
+       return response()->json([
+        "status"=>401,
+        "message"=>"failed",
+       ], 401);
     }
 
     //Fungsi untuk mendapatkan data user / read user
